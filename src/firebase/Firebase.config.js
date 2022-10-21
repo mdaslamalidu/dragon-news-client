370dd3b895ba003f08ccdb14c7b28f3b756e3133
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-21 19:22:29 +0600

    update env process

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REAECT_APP_apiKey,
-  authDomain: process.env.REAECT_APP_authDomain,
-  projectId: process.env.REAECT_APP_projectId,
-  storageBucket: process.env.REAECT_APP_storageBucket,
-  messagingSenderId: process.env.REAECT_APP_messagingSenderId,
-  appId: process.env.REAECT_APP_appId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain, 
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
